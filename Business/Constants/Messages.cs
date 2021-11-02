466026f172742c344502761bbb4e8a24ce5c1ae6
Furkan Yazar <frknyz1@outlook.com>
2021-11-02 16:13:07 +0300

    updated

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,13 @@
         public static string ProductUpdated = "Ürün güncellendi";
         public static string ProductNameAlreadyExists = "Ürün ismi zaten var";
         public static string CategoryLimitExceded = "Kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Yetkilendirme reddedildi";
+        public static string UserRegistered = "Kullanıcı kaydedildi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Hatalı şifre";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Erişim başarılı";
+        public static string Success = "İşlem başarılı";
     }
 }
